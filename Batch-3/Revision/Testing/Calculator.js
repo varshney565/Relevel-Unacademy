2c4e0c212f6ebdb80c27a2f426008696db9638ed
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2022-12-13 22:59:31 +0530

    SpyOnAndJestFn

---
+++ Batch-3/Revision/Testing/Calculator.js
@@ -0,0 +1,6 @@
+module.exports = {
+    add : (a,b)=>a+b,
+    mul : (a,b)=>a*b,
+    sub : (a,b)=>a-b,
+    div : (a,b)=>a/b
+};
--- Batch-3/Revision/Testing/test.js
+++ Batch-3/Revision/Testing/test.js
@@ -15,3 +15,18 @@ test("checking Both Objects",()=>{
     expect(obj).toEqual({one : 1,two : 2});
     // expect(obj).toBe({one : 1,two : 2});
 })
+
+const Calculator = require("./Calculator");
+
+test.only("testing SpyOn Method",()=>{
+    const mockCallback = jest.spyOn(Calculator,"add");
+    
+    /**
+     * By default mockCallback use the default implementation of the function
+     * that we have mocked
+     */
+    mockCallback.mockImplementation(()=>"Shivam");
+    const res = mockCallback(1,3);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+    expect(res).toBe("Shivam");
+});
