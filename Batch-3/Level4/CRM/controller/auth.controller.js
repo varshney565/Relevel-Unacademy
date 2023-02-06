5217b2846251ba45ca4ac4e8f5ebb441c13b421f
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-02-06 23:41:06 +0530

    AuthDOne

--- Batch-3/Level4/CRM/controller/auth.controller.js
+++ Batch-3/Level4/CRM/controller/auth.controller.js
@@ -23,6 +23,12 @@
 
 const {User} = require('../model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/server.secret');
+
+/**
+ * Logic for signup
+ */
 
 exports.signup = async (req,res)=>{
     /**
@@ -75,6 +81,81 @@ exports.signup = async (req,res)=>{
     }
 }
 
-exports.signin = (req,res)=>{
 
+/**
+ * Logic for signin
+ */
+
+exports.signin = async (req,res)=>{
+    /**
+     * I will be provided the userId and password.
+     */
+    const userData = {
+        userId : req.body.userId,
+        password : req.body.password
+    };
+
+    /**
+     * Whether userId that is provided is correct or not.
+     */
+    try{
+        const user = await User.findOne({userId : userData.userId});
+        if(!user){
+            console.log("No such user with this userId.");
+            res.staus(400).send({
+                message : "Failed! UserId does't exist."
+            })
+            return;
+        }
+        /**
+         * Whether password is correct or not.
+         */
+        const res = bcrypt.compareSync(userData.password,user.password);
+        if(!res){
+            console.log("Wrong Password Provided.");
+            res.staus(401).send({
+                message : "Wrong Password Provided."
+            })
+            return;
+        }
+        /**
+         * check if the userStatus is not PENDING is yes : he can't login.
+         */
+        if(user.userStatus == "PENDING"){
+            res.status(403).send({
+                message : "Not have permisions, must be approved by ADMIN first."
+            })
+            return;
+        }
+
+
+        /**
+         * Generate a Token.
+         */
+        const token = jwt.sign({id : user.userId,},secret,{
+            expiresIn : 600
+        });
+        /**
+         * Return the response.
+         */
+        const response = {
+            name  : user.name,
+            email : user.email,
+            userId : user.userId,
+            token : token,
+            userType : user.userType,
+            userStatus : user.userStatus,
+            createdAt : user.createdAt,
+            updatedAt : user.updatedAt
+        }
+        res.status(200).send({
+            message : "Successfully Logged In",
+            userDetails : response
+        })
+    }catch(err){
+        console.log("some error while logging in");
+        res.status(500).send({
+            message : err.name || "Internal Erorr"
+        });
+    }
 }
