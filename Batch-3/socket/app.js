e3c17a51446e3142765410a12db2c810c4c46950
Your Name <uietcse1923shivamvarsheny@kuk.ac.in>
2023-02-18 15:06:23 +0530

    SocketHttpAndSOcketConnectionDOne

--- Batch-3/socket/app.js
+++ Batch-3/socket/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+/**
+ * we need http server
+ */
+const server = require('http').Server(app);
+/**
+ * socket server is also needed on both client and server side.
+ */
+const socket = require('socket.io');
+const io = new socket.Server(server);
+
+/**
+ * when client is trying to connect to the server.
+ */
+
+io.on('connection',(socket)=>{
+    console.log("one client connected.");
+
+    //when the client is closed
+    socket.on('disconnect',()=>{
+        console.log("one client disconnected.")
+    });
+}); 
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname , "./index.html"));
 })
 
-app.listen(8080,()=>{
+server.listen(8080,()=>{
     console.log("server started running !!");
 })
